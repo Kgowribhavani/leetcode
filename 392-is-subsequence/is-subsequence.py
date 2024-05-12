97d99e23cbbd936754303b8e96190f2cae5aa064
Kgowribhavani <133024153+Kgowribhavani@users.noreply.github.com>
2024-05-12 06:49:17 +0530

    Time: 19 ms (24.04%) | Memory: 11.9 MB (39.36%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -12,15 +12,9 @@ class Solution(object):
         for i in range(len(s)):
             if s[i] in t[sub+1:] and sub+1!=len(t):
                 a=s[i]
-                print(a)
-                print('sub',sub+1)
                 sub=t.find(a,sub+1)
-                print('sub',sub+1)
                 b+=a
-                print(b)
                 if len(s)==len(b) and b==s:
                     return True
-            else:
-                return False
 
         
